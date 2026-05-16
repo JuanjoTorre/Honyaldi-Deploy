7b06290fd08edd4b64e25fa1a3ab724b59f16fb3
JuanjoTorre <jjtorregalan@gmail.com>
2026-05-16 21:35:21 +0200

    prueba de conexion

--- database/connection.js
+++ database/connection.js
@@ -1,28 +1,26 @@
- /********************************************************FICHERO DE CONEXION A LA BBDD
-******************************************************************************************/
+/********************************************************FICHERO DE CONEXION A LA BBDD
+ ******************************************************************************************/
 
- /********************************IMPORTAMOS MONGOOSE
-**********************************************************/
+/********************************IMPORTAMOS MONGOOSE
+ **********************************************************/
 const mongoose = require("mongoose");
 
+// Prueba de conexion
 
+/*********************METODO QUE CONECTA CON LA BBDD
+ **********************************************************/
+const connection = async () => {
+	try {
+		//Conexion a la base de datos que se llama 'honyaldi'
+		// await mongoose.connect("mongodb://localhost:27017/honyaldi");
+		await mongoose.connect(process.env.DB_CNN);
+		console.log("Conectado a db: honyaldi");
+	} catch (error) {
+		console.log(error);
+		throw new Error("No se ha podido conectar a la base de datos");
+	}
+};
 
-
- /*********************METODO QUE CONECTA CON LA BBDD
-**********************************************************/
-const connection = async() => {
-      try {
-            //Conexion a la base de datos que se llama 'honyaldi'
-            // await mongoose.connect("mongodb://localhost:27017/honyaldi");
-            await mongoose.connect(process.env.DB_CNN);
-            console.log('Conectado a db: honyaldi')
-      } catch (error) {
-           console.log(error);
-           throw new Error ('No se ha podido conectar a la base de datos') 
-      }
-}
-
-
- /******************************EXPORTAMOS LA CONEXION
-**********************************************************/
+/******************************EXPORTAMOS LA CONEXION
+ **********************************************************/
 module.exports = connection;
